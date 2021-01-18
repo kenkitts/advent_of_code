037b87e1a5ce3c1f87d568ac356f7ea2df4c2d83
Ken <46403824+kenkitts@users.noreply.github.com>
2021-01-18 11:30:58 -0700

    Day 15 optimizations

--- day15/day15p2.py
+++ day15/day15p2.py
@@ -2,34 +2,36 @@ import time
 
 start_time = time.time()
 
+target_turn = 30000000
+
 with open('input.txt','rt') as file:
-    data = file.read().split(',')
-    data_map = {}
+    data = (int(i) for i in file.read().split(','))
+    data_map = {**dict.fromkeys(range(0,target_turn + 1),None)}
     for value,key in enumerate(data):
         data_map[key] = value + 1
 
 
 def main():
-    turn = len(data) + 1
-    next_number = ''
+    turn = value + 2
+    next_number = 0
     old_turn = None
-    while turn <= 30000000:
+    while turn <= target_turn:
         if next_number in data_map.keys() and old_turn is not None:
-            next_number = str(data_map[next_number] - old_turn)
-            if str(next_number) in data_map.keys():
+            next_number = data_map[next_number] - old_turn
+            if data_map[next_number] is not None:
                 old_turn = data_map[next_number]
             else:
                 old_turn = None
             data_map[next_number] = turn
         else:
-            next_number = '0'
-            if str(next_number) in data_map.keys():
-                old_turn = data_map['0']
+            next_number = 0
+            if data_map[next_number] is not None:
+                old_turn = data_map[0]
             data_map[next_number] = turn
         turn +=1
     return next_number
 
-print(main())
+print('The solution is: {}'.format(main()))
 
 stop_time = time.time()
 print('Code took {} seconds to complete.'.format(stop_time - start_time))
